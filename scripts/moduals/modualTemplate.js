18fc9defec4f1afce3cf88ecba93f48aded5cf6d
Mark Spronck <markspronck@gmail.com>
2016-01-27 10:36:19 +0800

    Cleanup
    Cleanup

---
+++ scripts/moduals/modualTemplate.js
@@ -0,0 +1,21 @@
+(function(){  // **MODUAL** code will have use strit prefixed
+    // important do not add anything above this line. Modual loading adds (use strict) and accepts the returned value of this function
+    var yourObject = function(){
+        return {};
+    }
+    var configure = function(){
+        // add code here as a one off run to load any requiered moduals or stuff
+        // be aware that it is posible to create a cyclic recursion here if loading moduals that
+        // lead to referance to this modual so check if a modual has been loaded before you request them.
+        // May be fix but for now be warned.
+        
+    }
+    
+    return {
+        create : yourObject,  // requiered 
+        configure : configure,  // optional.
+    };
+})();
+    
+    
+    
